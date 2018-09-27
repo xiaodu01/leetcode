cde62097c44eb99d596ab25c10af9eba233d6a45
xiaodu01 <hao95163@163.com>
2018-09-27 22:28:14 +0800

    #104 Maximum Depth of Binary Tree

---
+++ src/java/easy/MaxDepth.java
@@ -0,0 +1,39 @@
+package java.easy;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class MaxDepth {
+
+    /**
+     * 描述：求二叉树的最大深度
+     * 如二叉树为
+     *          1
+     *         / \
+     *        2   2
+     *       / \  /\
+     *      3  4 4  3
+     *     /      \
+     *    4        8
+     * 的最大深度为 4
+     */
+
+    public int maxDepth(TreeNode root) {
+        return root == null ? 0 : (1 + Math.max(MaxDepth(root.left),MaxDepth(root.right)));
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
