f03d4586be7d558a3c5f767ab26e74ed3bf93d86
xiaodu01 <hao95163@163.com>
2018-10-09 21:22:58 +0800

    88 Merge Sorted Array

---
+++ src/java/easy/MergeSortedArray.java
@@ -0,0 +1,37 @@
+import com.sun.scenario.effect.Merge;
+
+public class MergeSortedArray {
+
+    /**
+     * 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+     * 说明:
+     * 
+     * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+     * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+     * 输入:
+     * nums1 = [1,2,3,0,0,0], m = 3
+     * nums2 = [2,5,6],       n = 3
+     * 输出: [1,2,2,3,5,6]
+     */
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int count = m + n - 1;
+        m--;
+        n--;
+        while ( m >= 0 && n >= 0 )
+            nums1[count--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        while ( n >= 0 )
+            nums1[count--] = nums2[n--];
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        int m = 3;
+        int n = 3;
+        new MergeSortedArray().merge(nums1, m, nums2, n);
+        for (int a : nums1) {
+            System.out.println(a);
+        }
+    }
+}
